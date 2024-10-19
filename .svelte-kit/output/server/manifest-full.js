396dfc42e142025371f0f41590086cc2d16dd00a
Irena Romendik <kidnemor@gmail.com>
2024-10-19 21:37:02 +0530

    added prompts

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["data/prompts.json","favicon.png","styles.css"]),
 	mimeTypes: {".json":"application/json",".png":"image/png",".css":"text/css"},
 	_: {
-		client: {"start":"app/immutable/entry/start.D73C9x1J.js","app":"app/immutable/entry/app.0CgKYkIZ.js","imports":["app/immutable/entry/start.D73C9x1J.js","app/immutable/chunks/entry.D29V-RB1.js","app/immutable/chunks/scheduler.BvLojk_z.js","app/immutable/entry/app.0CgKYkIZ.js","app/immutable/chunks/scheduler.BvLojk_z.js","app/immutable/chunks/index.DBrR8D_x.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"app/immutable/entry/start.D8SYUf23.js","app":"app/immutable/entry/app.CLesAQjt.js","imports":["app/immutable/entry/start.D8SYUf23.js","app/immutable/chunks/entry.DmjUYG2E.js","app/immutable/chunks/scheduler.BvLojk_z.js","app/immutable/entry/app.CLesAQjt.js","app/immutable/chunks/scheduler.BvLojk_z.js","app/immutable/chunks/index.DBrR8D_x.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
