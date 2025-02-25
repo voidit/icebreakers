f3d193fe0f2fb8f0270657937d917cc056508da5
Irena Romendik <kidnemor@gmail.com>
2025-02-26 00:52:26 +0530

    minor styles

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["bubble_wrap.mp3","data/prompts.json","favicon.png","styles.css"]),
 	mimeTypes: {".mp3":"audio/mpeg",".json":"application/json",".png":"image/png",".css":"text/css"},
 	_: {
-		client: {start:"app/immutable/entry/start.Csxkj4PX.js",app:"app/immutable/entry/app.CFlgDZt0.js",imports:["app/immutable/entry/start.Csxkj4PX.js","app/immutable/chunks/DplEX2U8.js","app/immutable/chunks/B2QSKqcC.js","app/immutable/entry/app.CFlgDZt0.js","app/immutable/chunks/B2QSKqcC.js","app/immutable/chunks/D4CtbWbK.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"app/immutable/entry/start.j4TjwvKN.js",app:"app/immutable/entry/app._ZqA-GY5.js",imports:["app/immutable/entry/start.j4TjwvKN.js","app/immutable/chunks/C5HoTWnN.js","app/immutable/chunks/B2QSKqcC.js","app/immutable/entry/app._ZqA-GY5.js","app/immutable/chunks/B2QSKqcC.js","app/immutable/chunks/D4CtbWbK.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
