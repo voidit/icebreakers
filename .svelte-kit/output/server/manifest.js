dcc4b994236fb178889045608542483f7e94f87b
Irena Romendik <kidnemor@gmail.com>
2025-02-27 17:27:36 +0530

    removed duplicate prompts

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -7,10 +7,10 @@ function __memo(fn) {
 return {
 	appDir: "app",
 	appPath: "app",
-	assets: new Set(["bubble_wrap.mp3","data/prompts.json","favicon.png","styles.css"]),
+	assets: new Set(["CNAME","bubble_wrap.mp3","data/prompts.json","favicon.png","styles.css"]),
 	mimeTypes: {".mp3":"audio/mpeg",".json":"application/json",".png":"image/png",".css":"text/css"},
 	_: {
-		client: {start:"app/immutable/entry/start.j4TjwvKN.js",app:"app/immutable/entry/app._ZqA-GY5.js",imports:["app/immutable/entry/start.j4TjwvKN.js","app/immutable/chunks/C5HoTWnN.js","app/immutable/chunks/B2QSKqcC.js","app/immutable/entry/app._ZqA-GY5.js","app/immutable/chunks/B2QSKqcC.js","app/immutable/chunks/D4CtbWbK.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"app/immutable/entry/start.CaO5rE9J.js",app:"app/immutable/entry/app.D1MkXX10.js",imports:["app/immutable/entry/start.CaO5rE9J.js","app/immutable/chunks/BIX_wMtm.js","app/immutable/chunks/B2QSKqcC.js","app/immutable/entry/app.D1MkXX10.js","app/immutable/chunks/B2QSKqcC.js","app/immutable/chunks/D4CtbWbK.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			
 		],
--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["app/immutable/nodes/1.u6JapLGG.js","app/immutable/chunks/B2QSKqcC.js","app/immutable/chunks/D4CtbWbK.js","app/immutable/chunks/C5HoTWnN.js"];
+export const imports = ["app/immutable/nodes/1.wSCKxG2Q.js","app/immutable/chunks/B2QSKqcC.js","app/immutable/chunks/D4CtbWbK.js","app/immutable/chunks/BIX_wMtm.js"];
 export const stylesheets = [];
 export const fonts = [];
